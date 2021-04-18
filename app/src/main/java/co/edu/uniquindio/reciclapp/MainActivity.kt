ec97ccf5e0b699abfdba25518fd90f15aa6f9e4f
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2021-04-17 21:15:33 -0500

    Login Fragment

--- app/src/main/java/co/edu/uniquindio/reciclapp/MainActivity.kt
+++ app/src/main/java/co/edu/uniquindio/reciclapp/MainActivity.kt
@@ -2,6 +2,7 @@ package co.edu.uniquindio.reciclapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
