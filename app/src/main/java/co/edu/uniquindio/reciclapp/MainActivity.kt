632e1d98214527201bf3cb61099cedd8374615f3
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2021-04-26 01:23:06 -0500

    Home Activity

--- app/src/main/java/co/edu/uniquindio/reciclapp/MainActivity.kt
+++ app/src/main/java/co/edu/uniquindio/reciclapp/MainActivity.kt
@@ -2,7 +2,6 @@ package co.edu.uniquindio.reciclapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
