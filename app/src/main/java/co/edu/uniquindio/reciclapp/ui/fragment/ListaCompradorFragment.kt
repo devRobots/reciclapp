263c711ff007d2765a7965541387501597aca2c1
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2021-07-04 22:24:37 -0500

    Conexion de lista compradores con BD

--- app/src/main/java/co/edu/uniquindio/reciclapp/ui/fragment/ListaCompradorFragment.kt
+++ app/src/main/java/co/edu/uniquindio/reciclapp/ui/fragment/ListaCompradorFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import co.edu.uniquindio.reciclapp.R
 import co.edu.uniquindio.reciclapp.adapter.ListCompradorAdapter
-import co.edu.uniquindio.reciclapp.model.Comprador
+import co.edu.uniquindio.reciclapp.data.RoomApp
+import kotlinx.coroutines.launch
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 /**
@@ -23,14 +24,13 @@ import kotlin.collections.ArrayList
 class ListaCompradorFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var roomApp: RoomApp
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_lista, container, false)
-        //Lista generica
         recyclerView = view.findViewById(R.id.rcvRetiroRealizado)
 
         return view
@@ -39,15 +39,16 @@ class ListaCompradorFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        roomApp = RoomApp(requireContext())
 
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(activity)
 
-        val elements = ArrayList<Comprador>()
-        elements.add( Comprador(1, "179537","Cristian Quiceno",3196681290,"ccquicenol@uqvirtual.edu.co"))
-        elements.add( Comprador(2, "179538","Joshua Quiceno",31543384895,"jjoshua@uqvirtual.edu.co"))
-        recyclerView.adapter = ListCompradorAdapter(elements, activity) {
-            findNavController().navigate(R.id.nav_comprador)
+        lifecycleScope.launch {
+            val elements = roomApp.admin.compradorDAO().obtenerTodas()
+            recyclerView.adapter = ListCompradorAdapter(elements, activity) {
+                findNavController().navigate(R.id.nav_comprador)
+            }
         }
     }
 }
