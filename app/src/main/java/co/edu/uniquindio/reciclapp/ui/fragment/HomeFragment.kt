0f9a55d49491ce7e74663e0db94ce71f01bba360
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2021-07-28 21:19:00 -0500

    FAB oculto segun contexto

--- app/src/main/java/co/edu/uniquindio/reciclapp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/co/edu/uniquindio/reciclapp/ui/fragment/HomeFragment.kt
@@ -6,11 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import co.edu.uniquindio.reciclapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val fab: FloatingActionButton? = activity?.findViewById(R.id.fab)
+        fab?.visibility = View.VISIBLE
     }
 
 }
--- app/src/main/java/co/edu/uniquindio/reciclapp/ui/fragment/PerfilFragment.kt
+++ app/src/main/java/co/edu/uniquindio/reciclapp/ui/fragment/PerfilFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import co.edu.uniquindio.reciclapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PerfilFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +14,11 @@ class PerfilFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_perfil, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val fab: FloatingActionButton? = activity?.findViewById(R.id.fab)
+        fab?.visibility = View.GONE
     }
 }
